e907f458092ad2dd0ea2538d82cef7073f9db91a
Eduarda Medeiros <eduarda_medeiros_silva@hotmail.com>
2024-06-02 13:46:01 -0300

    Atualizações

--- src/agent.py
+++ src/agent.py
@@ -1,3 +1,155 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0e7e51f638e3bf512a440a37788713c074f453f0eb74cf92781d8173766565be
-size 4900
+import csv
+import random
+import collections
+import pickle
+import os
+import numpy as np
+
+W = 'W'
+B = 'B'
+
+class Agent():
+    def __init__(self, alpha, gamma, eps, eps_decay=0.005):
+        self.alpha = alpha
+        self.gamma = gamma
+        self.eps = eps
+        self.eps_decay = eps_decay
+
+        self.player = None
+
+        self.Q = {}
+        self.actions = self.possible_actions( [[None] * 9 for _ in range(4)])   
+        for action in self.actions:
+            self.Q[action] = collections.defaultdict(int)
+      
+    def get_action(self, s):
+        string_s = self.get_state_key(s)
+        actions_s = self.possible_actions(s)
+
+        if random.random() < self.eps:
+            action = actions_s[random.randint(0,len(actions_s)-1)]
+        else:
+            values = np.array([self.Q[a][string_s] for a in actions_s])
+            ix_max = np.where(values == np.max(values))[0]
+
+            if len(ix_max) > 1:
+                ix_select = np.random.choice(ix_max, 1)[0]
+            else:
+                ix_select = ix_max[0]
+            action = actions_s[ix_select]
+
+        self.eps *= (1.-self.eps_decay)
+        return action
+    
+    def get_reward(self, game, a):
+        opponent = B if self.player == W else W
+        reward = 0
+
+        s = game.copy()
+        s.play(a)
+
+        if s.tie:
+            return 500
+        if s.win:
+            return 1000 if (self.player == W and game.white) or (self.player == B and game.black) else -1000
+
+        lines = s.get_lines()
+        updates = []
+        for line in lines:
+            if line not in game.get_lines():
+                updates.append(line)
+
+        for update in updates:
+            if s.get_sequences(update, opponent, 4):
+                reward -= 150
+                pass
+            elif s.get_sequences(update, opponent, 3):
+                reward -= 115
+                pass
+            elif s.get_sequences(update, 4 * opponent + self.player, 1) or (update, self.player + 4 * opponent, 1):
+                reward += 110
+                pass
+            elif s.get_sequences(update, 3* opponent + self.player, 1)  or (update, self.player + 3 * opponent, 1):
+                reward += 60
+                pass
+            elif s.get_sequences(update, self.player, 4):
+                reward += 100
+                pass
+            elif s.get_sequences(update, self.player, 3):
+                reward += 50
+                pass
+            elif s.get_sequences(update, self.player, 2):
+                reward += 25
+                pass
+            elif s.get_sequences(update, self.player, 1):
+                reward += 10
+                pass
+      
+        return reward
+
+    def get_state_key(self, s):
+        key = self.player + '-'
+        for row in s:
+            for elt in row:
+                if elt == None:
+                    elt = '*'
+                key += elt
+        return key
+
+    def update(self, s, s_, a, r):
+        string_s = self.get_state_key(s)
+        string_s_ = self.get_state_key(s_) if s_ is not None else None
+
+        if s_ is not None:
+            possible_actions = self.possible_actions(s_)
+            Q_options = [self.Q[action][string_s_] for action in possible_actions]
+            if Q_options:
+                self.Q[a][string_s] += self.alpha*(r + self.gamma*max(Q_options) - self.Q[a][string_s])
+            else:
+                self.Q[a][string_s] += self.alpha*(r - self.Q[a][string_s])
+        else:
+            self.Q[a][string_s] += self.alpha*(r - self.Q[a][string_s])
+
+    def possible_actions(self, s):
+        actions = []
+        for b in range(4):
+            for p in range(9):
+                for rb in range(4):
+                    if(s[b][p] == None):
+                        actions.append(str(b + 1) + "/" + str(p + 1) + " " + str(rb + 1) + "L")
+                        actions.append(str(b + 1) + "/" + str(p + 1) + " " + str(rb + 1) + "R")
+        return actions
+    
+    def play(self, game):
+        self.player = W if game.first_player_turn else B
+
+        s = game.blocks
+        a = self.get_action(s)
+        r = self.get_reward(game, a)
+
+        game.play(a)
+        
+        if not(game.win and game.tie):
+            s_ = game.blocks
+            self.update(s, s_, a, r)
+        else:
+            print(s)
+            self.update(s, None, a, r)
+
+        return a
+
+    def save(self, path = "agent_data.pkl"):
+        if os.path.isfile(path):
+            os.remove(path)
+        f = open(path, 'wb')
+        pickle.dump(self, f)
+        f.close()
+                    
+    def save_crv(self, filename='q_table.csv'):
+        states = list(next(iter(self.Q.values())).keys())
+        with open(filename, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([""] + states)
+
+            for actions, value in self.Q.items():
+                writer.writerow([actions] + [value[state] for state in states])
--- src/train.py
+++ src/train.py
@@ -1,3 +1,89 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9247c8c3df54739eceb5e0938c5d859fe7afdb43cd723f701905e643996635b7
-size 3101
+import random
+import pickle
+import os
+import pentago as Pentago
+import minimax as Minimax
+import agent as Agent
+
+class GameLearning():
+    def __init__(self, alpha=0.1, gamma=0.9, eps=1):
+        self.path = "agent_data.pkl"
+        self.txt = "log_train.txt"
+
+        if os.path.isfile(self.path):
+            self.size_path = os.path.getsize(self.path)
+            with open(self.path, 'rb') as f:
+                self.agent = pickle.load(f)
+        else:
+            self.size_path = 0
+            self.agent = Agent.Agent(alpha, gamma, eps)
+
+        self.minimax = Minimax.Minimax()
+
+        self.games_played = 0
+        self.ties = 0
+        self.agent_wins = 0
+        self.oponent_wins = 0
+
+    def begin_teaching(self, episodes):
+        while (self.games_played < episodes) and (self.size_path <= 1 * 1024 * 1024 * 1024):
+            self.games_played += 1
+            self.game = Pentago.Pentago()
+
+            print(f"============= INICIANDO JOGO {self.games_played} =============")
+            self.start()
+
+            self.size_path = os.path.getsize(self.path)
+            if self.games_played % 1000 == 0:
+                self.agent.save()
+                self.agent.save_crv()
+
+        self.agent.save()
+        self.agent.save_crv()
+
+        print("Vitórias Q-Learning: " + str(self.agent_wins))
+        print("Vitórias Oponente: " + str(self.oponent_wins))
+        print("Empates: " + str(self.ties))
+        
+    def log_text(self, txt):
+        if os.path.isfile(self.txt):
+            with open(self.txt, 'a') as f:
+                f.write(txt + "\n")
+
+    def start(self):
+        players = True if random.randint(0, 1) == 1 else False
+        order = ["Oponente", "Q-Learning"] if players else ["Q-Learning", "Oponente"]
+    
+        while (not self.game.win and not self.game.tie):
+            player = "Jogador 1 (" + '\033[37m' + '●' + '\033[0m' + ")" if self.game.first_player_turn else "Jogador 2 (" + '\033[30m' + '●' + '\033[0m' + ")"
+
+            if((order[0] == "Oponente" and self.game.first_player_turn) or (order[1] == "Oponente" and not self.game.first_player_turn)):
+                print(player, ": Oponente :", self.agent.play(self.game))
+            else:
+                print(player, ": Q-Learning :", self.agent.play(self.game))
+           
+        if (self.game.tie):
+            self.ties += 1
+            print("Houve empate!")
+            self.log_text(f"{self.games_played} - Empate N{self.ties}")
+        else:
+            if (self.game.white):
+                if(order[0] == "Oponente"):
+                    self.oponent_wins += 1
+                else:
+                    self.agent_wins += 1
+
+                print("O branco ganhou!") 
+                self.log_text(f"{self.games_played} - {order[0]}")
+            else:
+                if(order[1] == "Oponente"):
+                    self.oponent_wins += 1
+                else:
+                    self.agent_wins += 1
+
+                print("O preto ganhou!")
+                self.log_text(f"{self.games_played} - {order[1]}")
+            
+game = GameLearning()
+game.begin_teaching(1e5)
+
